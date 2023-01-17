b5d1a0fe9a14bbf268009d9c4576689dcd6fbf68
sudhesh99 <122530645+sudhesh99@users.noreply.github.com>
2023-01-17 12:09:05 +0530

    Creating a new file using file system with async await

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3030;
+const fs = require('fs')
 
 app.get('/', async(req,res)=>{
     try{
@@ -11,6 +12,8 @@ app.get('/', async(req,res)=>{
     }
 })
 
+
+//promise to display message
 function myMessage(message){
     console.log("My message is " + message);
 }
@@ -31,6 +34,20 @@ newPromise.then(
     }
 )
 
+//create file using fs module
+
+async function createMyFile(){
+    try{
+        return await fs.writeFile('myFile.txt', "Writting Something", function(err){
+            if(err) throw err;
+            console.log("File Created");
+        })
+    }catch(err){
+        console.log("Error", err)
+    }
+}
+
+createMyFile()
 
 app.listen(PORT, function(){
     console.log("Connected to PORT " + PORT)
